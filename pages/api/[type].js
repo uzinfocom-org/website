d2234e5f8828fc97e4b051cd5d65fd361cbef7f9
Yūri Katsuki <yuri@katsuki.moe>
2023-01-27 16:41:03 +0500

    removing a few unnecessary thingies

--- pages/api/[type].js
+++ pages/api/[type].js
@@ -1,19 +1,13 @@
-import design from '@data/design.json'
-import reading from '@data/useful.json'
-import music from '@data/music.json'
 import blog from '@data/blog.json'
 import keyboard from '@data/keyboards.json'
+import useful from '@data/useful.json'
 
 export default async function handler(req, res) {
   switch (req.query.type) {
     case 'keyboard':
       return res.status(200).json(keyboard)
-    case 'reading':
-      return res.status(200).json(reading)
-    case 'music':
-      return res.status(200).json(music)
-    case 'design':
-      return res.status(200).json(design)
+    case 'useful':
+      return res.status(200).json(useful)
     case 'blog':
       return res.status(200).json(blog)
     default:
